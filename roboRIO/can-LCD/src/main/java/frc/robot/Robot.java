e7fcb9e930dcbc4d7aa70d2e5557b315ee641696
Nathan zhou <nathan_zhou@outlook.com>
2025-05-29 21:46:22 -0400

    add battery example

--- roboRIO/can-LCD/src/main/java/frc/robot/Robot.java
+++ roboRIO/can-LCD/src/main/java/frc/robot/Robot.java
@@ -1,41 +1,28 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.ArduinoLCD;
 
 public class Robot extends TimedRobot {
 
     private ArduinoLCD lcd;
-    private String lastText = "";
+    private double lastVoltage = -1;
 
     @Override
     public void robotInit() {
         lcd = new ArduinoLCD(33); // LCD device number
-        SmartDashboard.putString("LCD Text", "Fennekin is so cute!");
-        SmartDashboard.putBoolean("LCD Clear", false);
     }
-    
 
-    
     @Override
     public void robotPeriodic() {
         lcd.processQueue();
-    
-        // Handle clear request
-        boolean clearRequested = SmartDashboard.getBoolean("LCD Clear", false);
-        if (clearRequested) {
-            lcd.clearScreen();
-            SmartDashboard.putBoolean("LCD Clear", false); // reset button
-        }
-    
-        // Handle text update
-        String currentText = SmartDashboard.getString("LCD Text", "");
-        if (!currentText.equals(lastText)) {
-            lcd.clearAndSend(currentText, 0, 0);
-            lastText = currentText;
+
+        double voltage = RobotController.getBatteryVoltage();
+        if (Math.abs(voltage - lastVoltage) > 0.01) { // update if significant change
+            String text = String.format("Battery: %.2f V", voltage);
+            lcd.send(String.format("%-20s", text), 0, 0);
+            lastVoltage = voltage;
         }
     }
-    
-    
-}
+} 
