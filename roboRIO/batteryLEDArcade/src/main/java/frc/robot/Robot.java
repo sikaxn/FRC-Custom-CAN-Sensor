535b747128f9519055b7f9eaab8b3817202c7f65
Nathan zhou <nathan_zhou@outlook.com>
2026-01-22 14:04:24 -0500

    Update Robot.java

--- roboRIO/batteryLEDArcade/src/main/java/frc/robot/Robot.java
+++ roboRIO/batteryLEDArcade/src/main/java/frc/robot/Robot.java
@@ -15,6 +15,10 @@ import frc.robot.subsystems.addressableLEDCAN;
 import frc.robot.subsystems.batteryCAN;
 
 public class Robot extends TimedRobot {
+  private static final int LED_DEFAULT_BRIGHTNESS = 128;
+  private static final int LED_DEFAULT_ON_OFF = 1;
+  private static final int LED_DEFAULT_TOTAL_PIXELS = 150;
+  private static final int LED_DEFAULT_PARAM1 = 20;
   private batteryCAN battery;
   private addressableLEDCAN leds;
   private SparkMax leftLeader;
@@ -34,25 +38,6 @@ public class Robot extends TimedRobot {
     SmartDashboard.putBoolean("ESP Reboot", false);
     SmartDashboard.putBoolean("Using RIO energy", false);
 
-    // General LED control
-    SmartDashboard.setDefaultNumber("LED Mode", 3);
-    SmartDashboard.setDefaultNumber("LED R", 255);
-    SmartDashboard.setDefaultNumber("LED G", 255);
-    SmartDashboard.setDefaultNumber("LED B", 255);
-    SmartDashboard.setDefaultNumber("LED Brightness", 128);
-    SmartDashboard.setDefaultNumber("LED OnOff", 1);
-    SmartDashboard.setDefaultNumber("LED Param0", 50);
-    SmartDashboard.setDefaultNumber("LED Param1", 20);
-    SmartDashboard.setDefaultNumber("LED Total Pixels", 150);
-
-    // Custom pixel write
-    SmartDashboard.setDefaultNumber("LED Pixel Index", 0);
-    SmartDashboard.setDefaultNumber("LED Pixel R", 255);
-    SmartDashboard.setDefaultNumber("LED Pixel G", 0);
-    SmartDashboard.setDefaultNumber("LED Pixel B", 0);
-    SmartDashboard.setDefaultNumber("LED Pixel Brightness", 128);
-    SmartDashboard.setDefaultBoolean("LED Write Pixel", false);
-
     leftLeader = new SparkMax(11, MotorType.kBrushed); // Left side leader
     leftFollower = new SparkMax(21, MotorType.kBrushed); // Left side follower
     rightLeader = new SparkMax(12, MotorType.kBrushed); // Right side leader
@@ -66,7 +51,7 @@ public class Robot extends TimedRobot {
 
     globalConfig
         .smartCurrentLimit(50)
-        .idleMode(IdleMode.kCoast);
+        .idleMode(IdleMode.kBrake);
 
     leftLeaderConfig
         .apply(globalConfig);
@@ -135,14 +120,17 @@ public class Robot extends TimedRobot {
     int r;
     int g;
     int b;
-    int brightness = (int) SmartDashboard.getNumber("LED Brightness", 128);
-    int onOff      = (int) SmartDashboard.getNumber("LED OnOff", 1);
+    int brightness = LED_DEFAULT_BRIGHTNESS;
+    int onOff      = LED_DEFAULT_ON_OFF;
     int param0;
     int param1;
-    int totalPixels = (int) SmartDashboard.getNumber("LED Total Pixels", 10);
+    int totalPixels = LED_DEFAULT_TOTAL_PIXELS;
 
-    double avgOutput = (leftLeader.getAppliedOutput() + rightLeader.getAppliedOutput()) / 2.0;
+    double leftOutput = leftLeader.getAppliedOutput();
+    double rightOutput = rightLeader.getAppliedOutput();
+    double avgOutput = (leftOutput + rightOutput) / 2.0;
     double speedAbs = Math.abs(avgOutput);
+    double turnDiff = Math.abs(leftOutput - rightOutput);
 
     if (!DriverStation.isEnabled()) {
       mode = 5;
@@ -151,6 +139,15 @@ public class Robot extends TimedRobot {
       r = 255;
       g = 255;
       b = 255;
+    } else if (turnDiff > 0.1) {
+      mode = 4;
+      double turnNorm = MathUtil.clamp((turnDiff - 0.1) / 1.9, 0.0, 1.0);
+      param0 = (int) Math.round(100.0 - (100.0 * turnNorm));
+      param0 = (int) MathUtil.clamp(param0, 0, 100);
+      param1 = LED_DEFAULT_PARAM1;
+      r = speedAbs > 0.2 && avgOutput < 0 ? 255 : 0;
+      g = speedAbs > 0.2 && avgOutput > 0 ? 255 : 0;
+      b = 0;
     } else if (speedAbs < 0.5) {
       mode = 13;
       param0 = (int) Math.round(30.0 - (20.0 * (speedAbs / 0.5)));
@@ -171,18 +168,15 @@ public class Robot extends TimedRobot {
     leds.setTotalPixel(totalPixels);
     leds.sendGeneralCommand(mode, r, g, b, brightness, onOff, param0, param1);
 
-    // Mode 255 Pixel write logic
-    boolean writePixel = SmartDashboard.getBoolean("LED Write Pixel", false);
-    if (writePixel) {
-      int index = (int) SmartDashboard.getNumber("LED Pixel Index", 0);
-      int pr    = (int) SmartDashboard.getNumber("LED Pixel R", 255);
-      int pg    = (int) SmartDashboard.getNumber("LED Pixel G", 0);
-      int pb    = (int) SmartDashboard.getNumber("LED Pixel B", 0);
-      int pbrig = (int) SmartDashboard.getNumber("LED Pixel Brightness", 128);
-
-      leds.sendPixelWrite(index, pr, pg, pb, 0, pbrig, 0); // w=0, slot=0
-      SmartDashboard.putBoolean("LED Write Pixel", false); // auto-reset trigger
-    }
+    SmartDashboard.putNumber("LED Mode", mode);
+    SmartDashboard.putNumber("LED R", r);
+    SmartDashboard.putNumber("LED G", g);
+    SmartDashboard.putNumber("LED B", b);
+    SmartDashboard.putNumber("LED Brightness", brightness);
+    SmartDashboard.putNumber("LED OnOff", onOff);
+    SmartDashboard.putNumber("LED Param0", param0);
+    SmartDashboard.putNumber("LED Param1", param1);
+    SmartDashboard.putNumber("LED Total Pixels", totalPixels);
 
     SmartDashboard.putNumber("Left Out", leftLeader.getAppliedOutput());
     SmartDashboard.putNumber("Right Out", rightLeader.getAppliedOutput());
@@ -190,8 +184,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    double speed = MathUtil.applyDeadband(-controller.getLeftY(), 0.08);
-    double rotation = MathUtil.applyDeadband(controller.getRightX(), 0.08);
+    double speed = MathUtil.applyDeadband(-controller.getLeftY(), 0.04);
+    double rotation = MathUtil.applyDeadband(-controller.getRightX(), 0.04);
     leftLeader.set(speed + rotation);
     rightLeader.set(speed - rotation);
   }
